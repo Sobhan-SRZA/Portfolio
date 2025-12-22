c9120474c41e7fdda4f322184df9c47e2b58ea64
Sobhan Rasoulzadeh Asl <sobhan.rasoulzadeh.asl@gmail.com>
2025-12-23 03:15:43 +0330

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -78,7 +78,7 @@ const Header: React.FC = () => {
 
     // Render the header with desktop and mobile navigation.
     return (
-        <header className="bg-[var(--sec-bg)] backdrop-blur-md text-[var(--text)] sticky top-0 left-0 w-full z-50 shadow-md ltr">
+        <header className="bg-(--sec-bg) backdrop-blur-md text-(--text) sticky top-0 left-0 w-full z-50 shadow-md ltr transition-colors">
             <nav
                 aria-label="Global" // Accessible label for navigation
                 className="mx-auto flex max-w-7xl items-center justify-between p-4 sm:p-6 lg:px-8"
@@ -87,13 +87,13 @@ const Header: React.FC = () => {
                 <div className="flex flex-1">
                     <NavLink
                         to="/" // Link to homepage
-                        className="text-2xl max-[1074px]:text-[18px] max-[1137px]:text-[22px] flex items-center space-x-2 space-x-reverse text-[var(--text)] hover:text-[var(--nav-hover)]"
+                        className="text-2xl max-[1074px]:text-[18px] max-[1137px]:text-[22px] flex items-center space-x-2 space-x-reverse text-(--text) hover:text-(--nav-hover)"
                     >
-                        <span className="font-bold tracking-tight transition-all">
+                        <span className="font-bold tracking-tight transition-colors">
                             Sobhan-SRZA
                         </span> {/* Primary part of the logo */}
-                        <span className="text-[var(--primary)]">/</span> {/* Separator */}
-                        <span className="ml-1 mr-1.5 font-bold tracking-tight transition-all">
+                        <span className="text-(--primary) transition-colors">/</span> {/* Separator */}
+                        <span className="ml-1 mr-1.5 font-bold tracking-tight transition-colors">
                             Mr. Sinre
                         </span> {/* Secondary part of the logo */}
                     </NavLink>
@@ -104,7 +104,7 @@ const Header: React.FC = () => {
                     <button
                         type="button"
                         onClick={() => setMobileMenuOpen(true)} // Open mobile menu
-                        className="inline-flex items-center justify-center rounded-md text-[var(--text)] hover:text-[var(--hover)] focus:outline-none transition-all p-0"
+                        className="inline-flex items-center justify-center rounded-md text-(--text) hover:text-(--hover) focus:outline-none transition-all p-0"
                         aria-label={mobileMenuOpen ? "Close menu" : "Open menu"} // Accessible label for screen readers
                     >
                         <Bars3Icon aria-hidden="true" className="h-8 w-8" /> {/* Hamburger icon */}
@@ -115,7 +115,7 @@ const Header: React.FC = () => {
                 <div className="hidden min-[1032px]:flex lg:gap-x-8">
                     <div
                         ref={highlightRef}
-                        className="absolute bg-[var(--nav-hover)] rounded-md transition-all duration-300 ease-in-out"
+                        className="absolute bg-(--nav-hover) rounded-md transition-all duration-300 ease-in-out"
                     />
 
                     {navItems.map((item, index) => (
@@ -126,7 +126,7 @@ const Header: React.FC = () => {
                                 if (isActive)
                                     setActiveItem(item.href)
 
-                                return `relative text-[17px] font-semibold px-3 py-2 rounded-md transition-colors duration-300 ${isActive ? "text-[var(--nav-text-hover)] cursor-not-allowed" : "text-[var(--text)] hover:text-[var(--nav-text-hover)] hover:bg-[var(--nav-hover)]"
+                                return `relative text-[17px] font-semibold px-3 py-2 rounded-md transition-colors duration-300 ${isActive ? "text-(--nav-text-hover) cursor-not-allowed" : "text-(--text) hover:text-(--nav-text-hover) hover:bg-(--nav-hover)"
                                     }`
                             }
                             }
@@ -146,7 +146,7 @@ const Header: React.FC = () => {
                 {/* Overlay for mobile menu */}
                 <div className="fixed inset-0 z-50 bg-black/30" aria-hidden="true" />
                 <DialogPanel
-                    className={`fixed inset-y-0 right-0 z-50 w-full sm:max-w-sm overflow-y-auto bg-[var(--sec-bg)] backdrop-blur-md p-6 transition-transform duration-300 transform ${mobileMenuOpen ? "translate-x-0" : "translate-x-full"}`}
+                    className={`fixed inset-y-0 right-0 z-50 w-full sm:max-w-sm overflow-y-auto bg-(--sec-bg) backdrop-blur-md p-6 transition-transform duration-300 transform ${mobileMenuOpen ? "translate-x-0" : "translate-x-full"}`}
                 >
                     {/* Mobile menu header with logo and close button */}
                     <div className="ltr flex items-center justify-between">
@@ -155,14 +155,14 @@ const Header: React.FC = () => {
                             onClick={() => setMobileMenuOpen(false)} // Close menu on click
                             className="flex items-center space-x-2 space-x-reverse"
                         >
-                            <span className="text-xl font-bold tracking-tight text-[var(--text)]">
+                            <span className="text-xl font-bold tracking-tight text-(--text)">
                                 Sobhan-SRZA / Mr. Sinre
                             </span> {/* Mobile menu logo */}
                         </NavLink>
                         <button
                             type="button"
                             onClick={() => setMobileMenuOpen(false)} // Close mobile menu
-                            className="rounded-md text-[var(--text)] hover:text-[var(--hover)] transition-all p-0"
+                            className="rounded-md text-(--text) hover:text-(--hover) transition-all p-0"
                             aria-label="Close menu" // Accessible label for screen readers
                         >
                             <XMarkIcon aria-hidden="true" className="h-8 w-8" /> {/* Close icon */}
@@ -176,7 +176,7 @@ const Header: React.FC = () => {
                                     key={item.name} // Unique key for each navigation item
                                     to={item.href} // Navigation route
                                     className={({ isActive }) =>
-                                        `text-center block rounded-lg px-3 py-2 text-base font-semibold text-[var(--text)] hover:bg-[var(--card-bg)]/50 ${isActive ? "text-[var(--primary)]" : ""} transition-all`
+                                        `text-center block rounded-lg px-3 py-2 text-base font-semibold text-(--text) hover:bg-(--card-bg)/50 ${isActive ? "text-(--primary)" : ""} transition-all`
                                     }
                                     onClick={() => setMobileMenuOpen(false)} // Close menu on click
                                 >
